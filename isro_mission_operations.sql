cc9749e142531e097830bf508e7d63ccf169e951
Thiru2010-coder <storagebackup369@gmail.com>
2026-07-19 09:31:41 +0530

    Added ISRO database table structures

---
+++ isro_mission_operations.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `admin` (
+  `AdminID` int NOT NULL AUTO_INCREMENT,
+  `Username` varchar(30) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `Name` varchar(50) NOT NULL,
+  `Role` varchar(30) DEFAULT 'Administrator',
+  PRIMARY KEY (`AdminID`),
+  UNIQUE KEY `Username` (`Username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
